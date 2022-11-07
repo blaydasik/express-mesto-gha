9d3c778eabf26c596137312930e34c2f86f918ff
Алексей Сергеевич Куров <alexei.s.kurov@gmail.com>
2022-11-07 17:01:00 +0300

    bugfix: getting user with unexisting id

--- controllers/users.js
+++ controllers/users.js
@@ -14,12 +14,19 @@ export function getUsers(req, res) {
 // обработчик запроса пользователя по id
 export function getUserById(req, res) {
   User.findById(req.params.userId)
-    .then((user) => res.send(user))
-    .catch((err) => {
-      // если пользователь не нашелся в БД, то ушипка 404
-      // поскольку mongoose выдает CastError, если не смогла найти пользователя в базе
-      if (err.name === 'CastError') {
+    .then((user) => {
+      //проверим, есть ли user в БД
+      if(user) {
+        res.send(user);
+      } else {
+        // если пользователь не нашелся в БД, то ушипка 404
         res.status(constants.HTTP_STATUS_NOT_FOUND).send({ message: `Пользователь по указанному _id=${req.params.userId} не найден.` });
+      }      
+    })
+    .catch((err) => {      
+      // если передан некорректный _id - ушипка 400
+      if (err.name === 'CastError') {
+        res.status(constants.HTTP_STATUS_BAD_REQUEST).send({ message: `Переданы некрректные данные: _id=${req.params.userId} при запросе информации о пользователе.` });
       } else {
         // 500 - ушипка по умолчанию
         res.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: 'На сервере произошла ошибка.' });
